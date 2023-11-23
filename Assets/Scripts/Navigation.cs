a96e75b8e4e0dd4d49533af7c74100a779647bdf
Shlombo <thomaszac28@aol.com>
2023-11-23 03:58:52 -0500

    Added Navigation

--- Assets/Scripts/Navigation.cs
+++ Assets/Scripts/Navigation.cs
@@ -1,18 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Navigation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // public static Navigation Instance;
+
+    public string lastScene;
+
+    public Room currentRoom;
+
+    public Room startRoom;
+
+    [Serializable]
+    public struct Room {
+        public string name;
+        public int index;
+    }
+    public Room[] roomList;
+    
+    private void Awake()
     {
-        
+        // if (Instance != null)
+        // {
+        //     Destroy(gameObject);
+        //     return;
+        // }
+        // Instance = this;
+
+        currentRoom = startRoom;
+
+
+        // DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void ZoomSlotGame() {
+        Debug.Log(SceneManager.GetActiveScene().name);
+        lastScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene("SlotGame");
+    }
+
+    public void LastScene() {
+        SceneManager.LoadScene(lastScene);
+        lastScene = null;
+    }
+
+    public void GoLeft() {
+        int idx = currentRoom.index - 1;
+        if(idx >= 0) {
+            currentRoom = roomList[idx];
+            SceneManager.LoadScene(roomList[idx].name);
+        }
+    }
+
+    public void GoRight() {
+        int idx = currentRoom.index + 1;
+        if(idx < roomList.Length) {
+            currentRoom = roomList[idx];
+            SceneManager.LoadScene(roomList[idx].name);
+        }
+    }
+
+    public void LoadRoom(string name) {
+        SceneManager.LoadScene(name);
     }
 }
